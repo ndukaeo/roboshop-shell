6d2bccd377203dec7fde99eb17aa93e6a721a97a
nduka ojeh <nojeh@baneollc.com>
2024-07-03 20:12:15 -0400

    updates catalogues script

---
+++ redis.sh
@@ -0,0 +1,8 @@
+dnf module disable redis -y
+dnf module enable redis:7 -y
+dnf install redis -y
+# Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis/redis.conf
+# Update protected-mode from yes to no in /etc/redis/redis.conf
+systemctl enable redis
+systemctl start redis
+
