54c0c1fe3c74d12e9bf7aa4890698287b7c361eb
nduka ojeh <nojeh@baneollc.com>
2024-07-04 10:59:47 -0400

    sed updates

--- redis.sh
+++ redis.sh
@@ -3,6 +3,8 @@ dnf module enable redis:7 -y
 dnf install redis -y
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis/redis.conf
 # Update protected-mode from yes to no in /etc/redis/redis.conf
+sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 systemctl enable redis
 systemctl start redis
 
