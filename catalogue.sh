fe083ff49b89450125a41b36663ca73af584b601
nduka ojeh <nojeh@baneollc.com>
2024-07-05 11:45:39 -0400

    added variables to roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,23 @@
 source common.sh
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+component=catalogue
 NODEJS
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongodb.dev.baneciollc.com </app/db/master-data.js
+
+
+
+#cp catalogue.service /etc/systemd/system/catalogue.service
+
+#useradd roboshop
+#rm -rf /app
+#mkdir /app
+#curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+#cd /app
+#unzip /tmp/catalogue.zip
+#cd /app
+#npm install
+#systemctl daemon-reload
+#systemctl enable catalogue
+#systemctl restart catalogue
