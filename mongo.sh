6fa58eced01b75b8522717225a1c52166eeb7228
nduka ojeh <nojeh@baneollc.com>
2024-07-06 19:01:25 -0400

    updated mongo

--- mongo.sh
+++ mongo.sh
@@ -1,7 +1,27 @@
+#cp mongo.repo /etc/yum.repos.d/mongo.repo
+#dnf install mongodb-org -y
+##Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+#sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+#systemctl enable mongod
+#systemctl restart mongod
+#
+
+source common.sh
+component=mongo
+
+PRINT copy mongodb repo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+STAT $?
+
+PRINT install mongodb
 dnf install mongodb-org -y
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+STAT $?
+
+PRINT Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+PRINT restart mongodb service
 systemctl enable mongod
 systemctl restart mongod
-
+STAT $?
