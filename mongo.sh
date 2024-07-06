361baed8a98337074e943ad6d30384f2245c37e1
nduka ojeh <nojeh@baneollc.com>
2024-07-06 19:44:13 -0400

    updated mongo

--- mongo.sh
+++ mongo.sh
@@ -10,18 +10,18 @@ source common.sh
 component=mongo
 
 PRINT copy mongodb repo file
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 STAT $?
 
 PRINT install mongodb
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOG_FILE
 STAT $?
 
 PRINT Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
 STAT $?
 
 PRINT restart mongodb service
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>$LOG_FILE
+systemctl restart mongod &>>$LOG_FILE
 STAT $?
--- redis.sh
+++ redis.sh
@@ -1,10 +1,36 @@
-dnf module disable redis -y
-dnf module enable redis:7 -y
-dnf install redis -y
+#dnf module disable redis -y
+#dnf module enable redis:7 -y
+#dnf install redis -y
+## Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis/redis.conf
+## Update protected-mode from yes to no in /etc/redis/redis.conf
+#sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+#sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+#systemctl enable redis
+#systemctl restart redis
+source common.sh
+component=redis
+
+PRINT disable default redis
+dnf module disable redis -y &>>$LOG_FILE
+STAT $?
+
+PRINT enable redis 7
+dnf module enable redis:7 -y &>>$LOG_FILE
+STAT $?
+
+PRINT install redis 7
+dnf install redis -y &>>$LOG_FILE
+STAT $?
+
+PRINT update redis config
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis/redis.conf
 # Update protected-mode from yes to no in /etc/redis/redis.conf
-sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-systemctl enable redis
-systemctl restart redis
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
+STAT $?
+
+PRINT redis service
+systemctl enable redis &>>$LOG_FILE
+systemctl restart redis &>>$LOG_FILE
+STAT $?
+
 
