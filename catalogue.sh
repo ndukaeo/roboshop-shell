a5ab65720462fbbabca1a703b6262e4b3b0350bd
nduka ojeh <nojeh@baneollc.com>
2024-07-05 20:55:23 -0400

    error or success codes

--- catalogue.sh
+++ catalogue.sh
@@ -9,11 +9,19 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo Install MongoDB Client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+    echo FAILURE
+fi
 
 echo Load Master Data
 mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js &>>$LOG_FILE
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+    echo FAILURE
+fi
 
 #cp catalogue.service /etc/systemd/system/catalogue.service
 
