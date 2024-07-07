0b36ed2294d78592ec920404fb0c8d6b949f2b65
nduka ojeh <nojeh@baneollc.com>
2024-07-06 23:36:19 -0400

    updating hosted zone addresses

--- catalogue.sh
+++ catalogue.sh
@@ -1,25 +1,25 @@
-#source common.sh
-#component=catalogue
-#app_path=/app
-#schema_setup=mongo
-#
-#NODEJS
+source common.sh
+component=catalogue
+app_path=/app
+schema_setup=mongo
+
+NODEJS
 
 
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
-dnf install mongodb-mongosh -y
-mongosh --host mongodb.dev.banecio-devops.online </app/db/master-data.js
+#cp mongo.repo /etc/yum.repos.d/mongo.repo
+#dnf module disable nodejs -y
+#dnf module enable nodejs:20 -y
+#dnf install nodejs -y
+#useradd roboshop
+#mkdir /app
+#curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+#cd /app
+#unzip /tmp/catalogue.zip
+#cd /app
+#npm install
+#systemctl daemon-reload
+#systemctl enable catalogue
+#systemctl start catalogue
+#dnf install mongodb-mongosh -y
+#mongosh --host mongodb.dev.banecio-devops.online </app/db/master-data.js
