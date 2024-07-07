c5b17bfe23cd257339b3737a86e1005d37bc0763
nduka ojeh <nojeh@baneollc.com>
2024-07-06 22:21:49 -0400

    dispatch NFR update

--- dispatch.sh
+++ dispatch.sh
@@ -1,27 +1,27 @@
-#source common.sh
-#component=dispatch
-#app_path=/app
+source common.sh
+component=dispatch
+app_path=/app
+
+GOLANG
+
+
+#dnf install golang -y
+#useradd roboshop
+#rm -rf /app
+#mkdir /app
+#curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+#cd /app
+#unzip /tmp/dispatch.zip
+#cd /app
+#go mod init dispatch
+#go get
+#go build
 #
-#GOLANG
-
-
-dnf install golang -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cd /app
-unzip /tmp/dispatch.zip
-cd /app
-go mod init dispatch
-go get
-go build
-
-
-cp dispatch.service /etc/systemd/system/dispatch.service
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl restart dispatch
-
-
-
+#
+#cp dispatch.service /etc/systemd/system/dispatch.service
+#systemctl daemon-reload
+#systemctl enable dispatch
+#systemctl restart dispatch
+#
+#
+#
