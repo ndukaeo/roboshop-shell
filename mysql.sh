9337b8a777a08686535bd0356c77fbfb7932e37a
nduka ojeh <nojeh@baneollc.com>
2024-07-06 20:31:10 -0400

    mysql update

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,15 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+source common.sh
+component=mysql
 
+PRINT Install mysql server
+dnf install mysql-server -y &>>$LOG_FILE
+STAT $?
+
+PRINT restart my sql serviue
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld &>>$LOG_FILE
+STAT $?
+
+PRINT set up mysql root password
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+STAT $?
