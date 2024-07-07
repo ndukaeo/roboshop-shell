d39744e6c8f876830eb376315419415c627fffb6
nduka ojeh <nojeh@baneollc.com>
2024-07-07 17:24:45 -0400

    fixing user add issue

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -15,11 +15,28 @@ systemctl enable rabbitmq-server &>>$LOG_FILE
 systemctl start rabbitmq-server &>>$LOG_FILE
 STAT $?
 
+#PRINT Adding Application User and password
+#rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+#STAT $?
+
 PRINT Adding Application User and password
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
 rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+fi
 STAT $?
+
 
 PRINT Setting user privileges
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 STAT $?
 
+
+
+
+
+#  id roboshop &>>$LOG_FILE
+#  if [ $? -ne 0 ]; then
+#  add_user roboshop roboshop123 &>>$LOG_FILE
+#  fi
+#  STAT $?
